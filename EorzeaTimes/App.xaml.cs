cc735545e20cbf5d04c61e2cac1446efbb7e79b7
Michael Bond <mbond@ltisolutions.com>
2022-01-06 12:00:01 -0500

    Add project files.

---
+++ EorzeaTimes/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EorzeaTimes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		MainPage = new MainPage();
+	}
+}
---
+++ EorzeaTimes/EorzeaDateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace EorzeaTimes;
+
+public static class EorzeaDateTimeExtensions
+{
+    public static DateTime ToEorzeaTime(this DateTime date)
+    {
+        const double EORZEA_MULTIPLIER = 3600D / 175D;
+
+        // Calculate how many ticks have elapsed since 1/1/1970
+        long epochTicks = date.ToUniversalTime().Ticks - (new DateTime(1970, 1, 1).Ticks);
+
+        // Multiply those ticks by the Eorzea multipler (approx 20.5x)
+        long eorzeaTicks = (long)Math.Round(epochTicks * EORZEA_MULTIPLIER);
+
+        return new DateTime(eorzeaTicks);
+    }
+}
